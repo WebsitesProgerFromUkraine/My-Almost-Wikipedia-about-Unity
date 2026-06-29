e46db898d16a5a2340411539ba7eec6cd59eb7b6
WebsitesProgerFromUkraine <lavrykpavlo245@gmail.com>
2026-06-29 09:14:39 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/facts")
+def facts():
+    return render_template('facts.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
